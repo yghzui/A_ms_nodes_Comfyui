8aa0020426397033e93d7e13eea8c6c0dee2ef59
yanggh <469033622@qq.com>
2025-07-31 01:49:01 +0800

    feat(A_my_nodes): 添加 load_lora_batch.js 功能
    - 在 A_my_nodes 扩展中引入 load_lora_batch.js 模块
    - 添加加载 LoRA 批处理功能

--- web/extensions/A_my_nodes/index.js
+++ web/extensions/A_my_nodes/index.js
@@ -1,8 +1,9 @@
 import "../../js/mask_add_dynamic.js";
 import "../../js/mask_subtract_dynamic.js";
 import "../../js/analyze_mask.js";
-
+import "../../js/load_lora_batch.js";
 console.log("A_my_nodes extensions loaded");
 console.log("Loaded mask_add_dynamic.js");
 console.log("Loaded mask_subtract_dynamic.js");
 console.log("Loaded analyze_mask.js"); 
+console.log("Loaded load_lora_batch.js");
